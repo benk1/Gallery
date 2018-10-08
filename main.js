a093b464edb65558179c651ca7bbdff5bc6339d5
benk1 <kakengi_bernard@hotmail.com>
2018-10-08 18:38:34 +0300

    add arrows

--- main.js
+++ main.js
@@ -15,7 +15,17 @@ document.querySelector('#Next').addEventListener('click', function(e) {
     getNextPhoto();
    });
 
+   document.querySelector('#next').addEventListener('click', function(e) {
+    
+    getNextPhoto();
+   });
+
    document.querySelector('#Prev').addEventListener('click', function(e) {
+    
+    getBackPhoto();
+   });
+
+   document.querySelector('#previous').addEventListener('click', function(e) {
     
     getBackPhoto();
    });
@@ -53,9 +63,10 @@ document.querySelector('#Next').addEventListener('click', function(e) {
     
     function getDetails(){
        array.forEach(person => {
+           
         //console.log(person.firstName);
            let dataDiv = document.querySelector("#data")
-           data.innerHTML = `FirstName: ${array[imageIndex].firstName}<br /> LastName: ${array[imageIndex].lastName} <br />Nationality: ${array[imageIndex].nationality}<br />Title: ${array[imageIndex].title}<br />Joined On: ${array[imageIndex].joinedOn}<br />Src: ${array[imageIndex].src}<br />Motivates me:  ${array[imageIndex].motivatesMe}` ;
+           data.innerHTML = `FirstName: ${array[imageIndex].firstName}<br /> LastName: ${array[imageIndex].lastName} <br />Nationality: ${array[imageIndex].nationality}<br />Title: ${array[imageIndex].title}<br />Joined On: ${array[imageIndex].joinedOn}<br />Src: ${array[imageIndex].src}<br />Motivates me:  ${array[imageIndex].motivatesMe}<br />Skills:  ${array[imageIndex].skills}` ;
           // console.log(`./images/${array[imageIndex].src}`);
           //data.appendChild(wrapper);
        })
