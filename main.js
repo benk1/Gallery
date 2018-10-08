f54c228c663bb72603997fa973be666a6a76e2ad
benk1 <kakengi_bernard@hotmail.com>
2018-10-08 13:42:27 +0300

    styling the Gallery

--- main.js
+++ main.js
@@ -7,24 +7,30 @@ let img = document.querySelector("#Image");
 let next = document.querySelector("#Next");
 //img.appendChild(wrapper);
 
-let details = document.querySelector("#Details");
-let detailsValue = details.value;
+//let details = document.querySelector("#Details");
+//let detailsValue = details.value;
 
 document.querySelector('#Next').addEventListener('click', function(e) {
     
     getNextPhoto();
    });
 
-    document.querySelector('#Details').addEventListener('click', function(e) {
+   document.querySelector('#Prev').addEventListener('click', function(e) {
+    
+    getBackPhoto();
+   });
+
+    /*document.querySelector('#Details').addEventListener('click', function(e) {
         getDetails();
         
-    });
+    });*/
 
     let imageIndex = 0;
     
     function getNextPhoto(){   
-        if(imageIndex > (array.length -1)){
+        if(imageIndex > (array.length )){
             imageIndex = 0;
+           
         }else {
            imageIndex++;
         }
@@ -32,19 +38,31 @@ document.querySelector('#Next').addEventListener('click', function(e) {
        img.src = `./images/${array[imageIndex].src}`;
        getDetails();
       }
+
+     function getBackPhoto() {
+        if(imageIndex === (array.length - 1)){
+            //imageIndex = 0;
+        }else {
+           imageIndex--;
+        }
+
+       img.src = `./images/${array[imageIndex].src}`;
+       getDetails();
+      
+     }
     
     function getDetails(){
        array.forEach(person => {
         //console.log(person.firstName);
            let dataDiv = document.querySelector("#data")
-           data.innerHTML = `FirstName: ${array[imageIndex].firstName}<br /> LastName: ${array[imageIndex].lastName} <br />Nationality: ${array[imageIndex].nationality}<br />Title: ${array[imageIndex].title}<br />Joine On: ${array[imageIndex].joinedOn}<br />${array[imageIndex].src}` ;
+           data.innerHTML = `FirstName: ${array[imageIndex].firstName}<br /> LastName: ${array[imageIndex].lastName} <br />Nationality: ${array[imageIndex].nationality}<br />Title: ${array[imageIndex].title}<br />Joined On: ${array[imageIndex].joinedOn}<br />Src: ${array[imageIndex].src}<br />Motivates me:  ${array[imageIndex].motivatesMe}` ;
           // console.log(`./images/${array[imageIndex].src}`);
           //data.appendChild(wrapper);
        })
     }
     
     img.src = `./images/${array[imageIndex].src}`;
-    getDetails();
+    getDetails(); 
      
     
 
